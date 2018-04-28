7a71f84f08166f03f144b241f0926884359b7d38
Alexander <alex.gavriliak@gmail.com>
2018-04-28 23:43:32 +0300

    A few refactore

--- Task1KodiSoft/Controllers/AccountController.cs
+++ Task1KodiSoft/Controllers/AccountController.cs
@@ -215,22 +215,6 @@ namespace Task1KodiSoft.Controllers
             return View();
         }
 
-        private string CalculateMD5Hash(string input)
-        {
-            // step 1, calculate MD5 hash from input
-            MD5 md5 = System.Security.Cryptography.MD5.Create();
-            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
-            byte[] hash = md5.ComputeHash(inputBytes);
-            // step 2, convert byte array to hex string
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < hash.Length; i++)
-            {
-                sb.Append(hash[i].ToString("X2"));
-            }
-            return sb.ToString();
-        }
-
-
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
@@ -269,6 +253,22 @@ namespace Task1KodiSoft.Controllers
             // If we got this far, something failed, redisplay form
             return View(model);
         }
+
+        private string CalculateMD5Hash(string input)
+        {
+            // step 1, calculate MD5 hash from input
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+            // step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
 
         [HttpPost]
         [ValidateAntiForgeryToken]
